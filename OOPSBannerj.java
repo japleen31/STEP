dc97de42398132878447d27d73f3e993e29221e6
japleen31 <japleenkaur3106@gmail.com>
2026-02-23 21:22:38 +0530

    Add files via upload

---
+++ OOPSBannerj.java
@@ -0,0 +1,15 @@
+ public class OOPSBannerj {
+    public static void main(String[] args) {
+
+    
+
+        // Each row of letters (9 width per letter)
+        System.out.println("  *****  " + "  *****  " + " ******* " + " ******* ") ;
+        System.out.println(" *     * " + " *     * " + " *     * " + " *       ");
+        System.out.println(" *     * " + " *     * " + " *     * " + " *       ");
+        System.out.println(" *     * " + " *     * " + " ******* " + " ******* ");
+        System.out.println(" *     * " + " *     * " + " *       " + "       * ");
+        System.out.println(" *     * " + " *     * " + " *       " + "       * ");
+        System.out.println("  *****  " + "  *****  " + " *       " + " ******* ");
+    }
+} 
