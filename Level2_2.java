3d5b990460b9eff7cdad2300f08edb70a7e8e433
Japleen Kaur <japleenkaur3106@gmail.com>
2026-03-02 09:26:13 +0530

    Add files via upload

---
+++ Level2_2.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+class Level2_2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter base in cm: ");
+        double base = sc.nextDouble();
+
+        System.out.print("Enter height in cm: ");
+        double height = sc.nextDouble();
+
+        double areaSqCm = 0.5 * base * height;
+
+        double areaSqIn = areaSqCm / (2.54 * 2.54);
+
+        System.out.println(
+            "The Area of the triangle in sq in is " + areaSqIn +
+            " and sq cm is " + areaSqCm
+        );
+
+        sc.close();
+    }
+}
---
+++ Level3_2.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+class Level3_2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter temperature in Fahrenheit: ");
+        double fahrenheit = sc.nextDouble();
+
+        double celsiusResult = (fahrenheit - 32) * 5 / 9;
+
+        System.out.println("The " + fahrenheit + " fahrenheit is " + celsiusResult + " celsius");
+
+        sc.close();
+    }
+}
